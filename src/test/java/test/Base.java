ac761019c55cbee66d0de5005ee2bbb0d8eb2d6b
Priyanka Negi <pnegi@cloudbees.com>
2025-03-12 13:05:57 +0530

    changes

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -6,7 +6,8 @@ public class Base {
 
     protected void run() {
         // Get the branch name or test behavior from a system property or environment variable
-        String branchBehavior = System.getProperty("branch.behavior", "fail"); // Default to "fail"
+        String branchBehavior = System.getProperty("branch.behavior", "fail");
+        System.out.println(branchBehavior);// Default to "fail"
 
         switch (branchBehavior) {
             case "fail":
